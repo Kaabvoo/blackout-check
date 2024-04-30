b3dc0a1dfe50d81118421eb7aea50aecccdf7eaf
Jose Cavazos <jose.cavazos@clubers.com>
2024-04-30 15:13:55 -0600

    Initial commit

---
+++ db/datasource.ts
@@ -0,0 +1,13 @@
+import { DataSource, DataSourceOptions } from "typeorm";
+
+export const options: DataSourceOptions = {
+    type: 'sqlite',
+    database: 'dist/db/data/data.db',
+    entities: ['dist/**/**/*.entity{.js, .ts}'],
+    migrations: ['dist/db/migrations/*.js'],
+    synchronize: false
+}
+
+const source = new DataSource(options);
+
+export default source;
